227e7429a67dc21d864240bb12d359950ebd3222
Mark Uvari <muvari@gmail.com>
2020-02-23 09:55:44 -0500

    Undo

--- src/CustomBot.js
+++ src/CustomBot.js
@@ -5,7 +5,7 @@ class CustomBot extends MCTSBot {
     ai.iterations = 1000;
     ai.playoutDepth = 5;
     ai.objectives = (G) => ({
-      'better than other player': {
+      'better than player 1': {
         checker: (G, ctx) => {
           return ((ctx.turn % ctx.numPlayers === 1) && G.scores[1] > G.scores[0]);
         },
@@ -22,6 +22,12 @@ class CustomBot extends MCTSBot {
 
     if (result.score !== undefined) {
       node.value += result.score;
+      if (result.score > 256)
+        node.value += 256;
+    }
+
+    if (result.draw === true) {
+      node.value += 50;
     }
     
     if (node.parent) {
